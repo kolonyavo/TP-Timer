08461ac6f9877176af486de32d9ca71f5b4de0e3
kolonyavo <hei.kolo.17@gmail.com>
2022-06-09 09:28:41 +0300

    delete unneeded imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import './App.css';
-import Timer from './Clock/Countdown';
-import {ClockClassComponent } from "./Clock/Clock";
 import React, { Component } from "react";
 import Hide from './Clock/Hidden';
-import Test from './Clock/Test';
-import { MyComponent } from './Clock/Content';
+
 
 function App() {
   return (
